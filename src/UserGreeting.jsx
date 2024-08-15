4748b7dbdd1e6786df6787d600a9c0f4b8e7369d
EricShadow <ldn.ericcreton@gmail.com>
2024-08-15 06:23:57 +0200

    User greeting (bool)

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -4,11 +4,9 @@ import PropTypes from "prop-types"
 
 function UserGreeting(props) {
 
-    if(props.isLoggedIn){
-        return <h2>Welcom{props.username}</h2>
-    }else {
-        return <h2>Please log in to continue !!</h2>
-    }
+    return (props.isLoggedIn ? 
+    <h2 className="welcome-message">Welcom {props.username}</h2> : 
+    <h2 className="login-prompt">Please log in to continue</h2>)
 
 }
 UserGreeting.propTypes={
